db7db368f4044fb5c906e030a8299027d8f539cf
wongso-kevin <54857392+wongso-kevin@users.noreply.github.com>
2024-06-28 01:10:42 -0400

    update init.lua to use nightlfy and commented out in colorschemes.lua

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,10 +1,11 @@
 -- lua for all custom color schemes
 
 return {
-    { 'catppuccin/nvim', lazy = true },
-    { 'comfysage/twilight-moon', lazy = true },
-    { 'oahlen/iceberg.nvim', lazy = true },
-    { "bluz71/vim-nightfly-colors", lazy = true },
-    { "EdenEast/nightfox.nvim", lazy = true }, -- lazy
-    { "olimorris/onedarkpro.nvim", lazy = true }
+  { 'catppuccin/nvim', lazy = true },
+  { 'folke/tokyonight.nvim', lazy = true },
+  { 'comfysage/twilight-moon', lazy = true },
+  { 'oahlen/iceberg.nvim', lazy = true },
+  -- { 'bluz71/vim-nightfly-colors', lazy = true },
+  { 'EdenEast/nightfox.nvim', lazy = true }, -- lazy
+  { 'olimorris/onedarkpro.nvim', lazy = true },
 }
